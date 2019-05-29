72ccbc95f7fc095f7d6f8537c4695e0d80845f90
gizmeondev <firoze@gizmeon.com>
2019-05-29 12:12:53 +0530

    Initial commit

---
+++ TestSDK/StringGenerator.swift
@@ -0,0 +1,15 @@
+//
+//  StringGenerator.swift
+//  TestSDK
+//
+//  Created by Firoze Moosakutty on 29/05/19.
+//  Copyright © 2019 Firoze Moosakutty. All rights reserved.
+//
+
+import Foundation
+
+public class StringGenerator {
+  public static func getString() -> String {
+    return "SDK String"
+  }
+}
---
+++ TestSDK/TestSDK.h
@@ -0,0 +1,19 @@
+//
+//  TestSDK.h
+//  TestSDK
+//
+//  Created by Firoze Moosakutty on 29/05/19.
+//  Copyright © 2019 Firoze Moosakutty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestSDK.
+FOUNDATION_EXPORT double TestSDKVersionNumber;
+
+//! Project version string for TestSDK.
+FOUNDATION_EXPORT const unsigned char TestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestSDK/PublicHeader.h>
+
+
